7b2d3f003adc00b3d09286fadff3b025f5fc36eb
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-02-25 23:31:10 +0100

    NEW: First Commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:api_login_app/widgets/circle.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Stack(
+          children: [CircleWidget(size: 200.0)],
+        ),
+      ),
+    );
+  }
+}
