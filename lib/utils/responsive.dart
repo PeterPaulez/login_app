b3590913d310bcb84068fa858e163027e7369df5
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-03-03 20:43:28 +0100

    CHANGE: Terminamos los retoques para el diseño del formulario, utilizamos una nueva propiedad para saber si el dispositivo es una Tableta

--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -3,6 +3,7 @@ import 'dart:math' as math;
 
 class Responsive {
   double _width, _height, _diagonal;
+  bool _isTablet;
 
   static Responsive of(BuildContext context) => Responsive(context);
 
@@ -11,11 +12,14 @@ class Responsive {
     this._width = size.width;
     this._height = size.height;
     this._diagonal = math.sqrt(math.pow(_width, 2) + math.pow(_height, 2));
+    this._isTablet =
+        size.shortestSide >= 600; // La media en alto y ancho mas pequeña
   }
 
   double get width => this._width;
   double get height => this._height;
   double get diagonal => this._diagonal;
+  bool get isTablet => this._isTablet;
 
   double withPercent(double percent) => this._width * percent / 100;
   double heightPercent(double percent) => this._height * percent / 100;
--- lib/widgets/loginForm.dart
+++ lib/widgets/loginForm.dart
@@ -14,84 +14,88 @@ class _LoginFormWidgetState extends State<LoginFormWidget> {
   Widget build(BuildContext context) {
     final Responsive responsive = Responsive.of(context);
     return Positioned(
-      bottom: 50,
-      left: 20, // Esto es para que el COLUMN pueda calcular el ancho
-      right: 20, // Esto es para que el COLUMN pueda calcular el ancho
-      child: Column(
-        children: <Widget>[
-          InputTextWidget(
-            labelText: 'EMAIL ADDRESS',
-            keyboardType: TextInputType.emailAddress,
-            fontSize: responsive.diagonalPercent(1.4),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.black12),
+      bottom: 30,
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: responsive.isTablet ? 430 : 360,
+        ),
+        child: Column(
+          children: <Widget>[
+            InputTextWidget(
+              labelText: 'EMAIL ADDRESS',
+              keyboardType: TextInputType.emailAddress,
+              fontSize: responsive.diagonalPercent(1.4),
+            ),
+            SizedBox(height: responsive.diagonalPercent(2)),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.black12),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: InputTextWidget(
+                      labelText: 'PASSWORD',
+                      obscureText: true,
+                      borderEnabled: false,
+                      fontSize: responsive.diagonalPercent(1.4),
+                    ),
+                  ),
+                  MaterialButton(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        fontSize: responsive.diagonalPercent(1.5),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: InputTextWidget(
-                    labelText: 'PASSWORD',
-                    obscureText: true,
-                    borderEnabled: false,
-                    fontSize: responsive.diagonalPercent(1.4),
+            SizedBox(height: responsive.diagonalPercent(5)),
+            MaterialButton(
+              padding: EdgeInsets.symmetric(
+                  vertical: responsive.diagonalPercent(1.5)),
+              minWidth: double.infinity,
+              onPressed: () {},
+              child: Text(
+                'Sign in',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: responsive.diagonalPercent(1.5)),
+              ),
+              color: Colors.pinkAccent,
+            ),
+            SizedBox(height: responsive.diagonalPercent(2)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'New to Friendly Desi?',
+                  style: TextStyle(
+                    fontSize: responsive.diagonalPercent(1.5),
                   ),
                 ),
                 MaterialButton(
-                  padding: EdgeInsets.symmetric(vertical: 10),
                   onPressed: () {},
                   child: Text(
-                    'Forgot Password',
+                    'Sign up',
                     style: TextStyle(
+                      color: Colors.pinkAccent,
                       fontSize: responsive.diagonalPercent(1.5),
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ],
             ),
-          ),
-          SizedBox(height: responsive.diagonalPercent(5)),
-          MaterialButton(
-            padding:
-                EdgeInsets.symmetric(vertical: responsive.diagonalPercent(1.5)),
-            minWidth: double.infinity,
-            onPressed: () {},
-            child: Text(
-              'Sign in',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: responsive.diagonalPercent(1.5)),
-            ),
-            color: Colors.pinkAccent,
-          ),
-          SizedBox(height: responsive.diagonalPercent(2)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'New to Friendly Desi?',
-                style: TextStyle(
-                  fontSize: responsive.diagonalPercent(1.5),
-                ),
-              ),
-              MaterialButton(
-                onPressed: () {},
-                child: Text(
-                  'Sign up',
-                  style: TextStyle(
-                    color: Colors.pinkAccent,
-                    fontSize: responsive.diagonalPercent(1.5),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: responsive.diagonalPercent(10)),
-        ],
+            SizedBox(height: responsive.diagonalPercent(10)),
+          ],
+        ),
       ),
     );
   }
